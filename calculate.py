3196b11cca5e2f301cf12602a73677006abacd6c
janakoyel39-afk <janakoyel39@gmail.com>
2026-09-18 09:22:18 +0530

    Initial commit

---
+++ calculate.py
@@ -0,0 +1,3 @@
+class calculate:
+    def add(self, a, b):
+        return a + b
---
+++ calculate2.py
@@ -0,0 +1,4 @@
+from calculate import calculate
+cal=calculate()
+result=cal.add(10,20)
+print("sum of 10 and 20 is:",result)
